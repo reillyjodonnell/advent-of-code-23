16e14b010d17bcdd6e82656e9ed2a3e121659f3f
Reilly O'Donnell <odonnell.reilly.j@gmail.com>
2023-12-17 16:09:56 -0500

    Cleaned up a lil

--- day-3/main.zig
+++ day-3/main.zig
@@ -4,54 +4,8 @@ const ArrayList = std.ArrayList;
 const reader = @import("reader.zig");
 const test_allocator = std.testing.allocator;
 const sum = @import("sum.zig");
-// read line until you hit a number
-// look right and add to list until no more number.
-
-// for each of the numbers check 8 directions around it for a symbol.
-// if not discard the number
-// push the number to a list
-// repeat for every line
 
 pub fn main() !void {}
-
-// turn 2d canvas to 1d array
-// group numbers
-//    -- how?
-//    receive position in array and move to the left until no more, move right until no more. Merge the u8s
-// check 8 directions and check if there's a symbol.
-// if so keep track and push to array
-// sum items in array
-
-// test "find numbers in array" {
-//     const example = "......644.....23..432";
-//     const expected = [_]u10{ 644, 23, 432 };
-
-//     const actual = try findNumbers(test_allocator, example);
-//     defer actual.deinit();
-
-//     for (actual.items, 0..) |number, index| {
-//         try expect(number == expected[index]);
-//     }
-// }
-
-fn findNumbers(allocator: std.mem.Allocator, num: []const u8) !ArrayList(u10) {
-    var joined = ArrayList(u10).init(allocator);
-
-    var number: u10 = 0;
-
-    for (num) |value| {
-        if (isNumber(value)) {
-            number = number * 10 + value - '0';
-            continue;
-        }
-        if (number > 0 and !isNumber(value)) {
-            try joined.append(number);
-            number = 0;
-        }
-        continue;
-    }
-    return joined;
-}
 
 fn isNumber(input: anytype) bool {
     const inputType = @TypeOf(input);
@@ -73,35 +27,26 @@ test "detects symbol in 8 directions around text" {
     const text = try reader.readFromFile(test_allocator, "input.txt");
     defer test_allocator.free(text);
     var items = try numbersAroundTokens(test_allocator, text);
-    items.deinit();
-    // var iterator = items.map.iterator();
+    defer items.deinit();
+    var iterator = items.map.iterator();
 
-    // var have_matching = ArrayList(u32).init(test_allocator);
-    // _ = have_matching;
+    var total_sum: u32 = 0;
 
-    // var total_sum: u32 = 0;
+    while (iterator.next()) |item| {
+        const numbersList = item.value_ptr; // The ArrayList of numbers
 
-    // while (iterator.next()) |item| {
-    //     const key = item.key_ptr;
-    //     _ = key; // The key
-    //     const numbersList = item.value_ptr; // The ArrayList of numbers
+        var local_sum: u32 = 1;
 
-    //     // Print the key
-    //     std.debug.print("\nvalue len: {d}\n", .{numbersList.items.len});
+        if (numbersList.items.len == 1) continue;
+        // Iterate over the list of numbers and print each one
+        for (numbersList.items) |num| {
+            local_sum = local_sum * num;
+        }
+        total_sum = total_sum + if (local_sum > 0) local_sum else 0;
 
-    //     var local_sum: u32 = 1;
-
-    //     if (numbersList.items.len == 1) continue;
-    //     // Iterate over the list of numbers and print each one
-    //     for (numbersList.items) |num| {
-    //         local_sum = local_sum * num;
-    //     }
-    //     total_sum = total_sum + if (local_sum > 0) local_sum else 0;
-
-    //     local_sum = 1;
-    // }
-
-    // std.debug.print("\ntotal: {d}\n", .{total_sum});
+        local_sum = 1;
+    }
+    try expect(total_sum == 73074886);
 }
 
 const NumberDict = struct {
@@ -165,149 +110,204 @@ fn createCoordKey(allocator: std.mem.Allocator, coord: Coordinate) ![]u8 {
     return std.fmt.allocPrint(allocator, "{},{}", .{ coord.x, coord.y });
 }
 
-fn numbersAroundTokens(allocator: std.mem.Allocator, passed: []const u8) !NumberDict {
-    var dictionary = NumberDict.init(allocator);
-    const arr = createArrayOfCoordinates(passed);
-    var has_symbol = false;
-    var number: u10 = 0;
+test "return coordinates of symbol around character" {
+    // the 7th character
+    var col: usize = 6;
+    var row: usize = 0;
+
+    const text = try reader.readFromFile(test_allocator, "input.txt");
+    defer test_allocator.free(text);
+
+    const arr = createArrayOfCoordinates(text);
+
+    const symbol_coords = getCoordinatesForNearestSymbol(row, col, arr);
+
+    var expected = [2]u8{ 5, 1 };
+
+    if (symbol_coords[0]) |x| {
+        try expect(x == expected[1]);
+    }
+
+    if (symbol_coords[1]) |y| {
+        try expect(y == expected[0]);
+    }
+}
+
+fn getCoordinatesForNearestSymbol(row_index: usize, col_index: usize, arr: [150][150]u8) [2]?u8 {
     var symbol_coordinates = [2]?u8{ null, null };
 
-    // create a dictionary where the coordinates of the shared symbol are the key and the numbers are the value
+    // arr[y][x] where the first arr is the row, or y axis, second is the col, or x axis.
+
+    if (row_index > 0 and col_index > 0) {
+        const top_left = arr[row_index - 1][col_index - 1];
+        if (isSymbol(top_left)) {
+            symbol_coordinates[0] = usizeToU8(row_index - 1).?;
+            symbol_coordinates[1] = usizeToU8(col_index - 1).?;
+        }
+    }
+    if (row_index > 0) {
+        const top_middle = arr[row_index - 1][col_index];
+        if (isSymbol(top_middle)) {
+            symbol_coordinates[0] = usizeToU8(row_index - 1).?;
+            symbol_coordinates[1] = usizeToU8(col_index).?;
+        }
+    }
+    if (col_index < 139 and row_index > 0) {
+        const top_right = arr[row_index - 1][col_index + 1];
+        if (isSymbol(top_right)) {
+            symbol_coordinates[0] = usizeToU8(row_index - 1).?;
+            symbol_coordinates[1] = usizeToU8(col_index + 1).?;
+        }
+    }
+    if (col_index > 0) {
+        const middle_left = arr[row_index][col_index - 1];
+        if (isSymbol(middle_left)) {
+            symbol_coordinates[0] = usizeToU8(row_index).?;
+            symbol_coordinates[1] = usizeToU8(col_index - 1).?;
+        }
+    }
+    if (col_index < 139) {
+        const middle_right = arr[row_index][col_index + 1];
+        if (isSymbol(middle_right)) {
+            symbol_coordinates[0] = usizeToU8(row_index).?;
+            symbol_coordinates[1] = usizeToU8(col_index + 1).?;
+        }
+    }
+    if (row_index < 139 and col_index > 0) {
+        const bottom_left = arr[row_index + 1][col_index - 1];
+        if (isSymbol(bottom_left)) {
+            symbol_coordinates[0] = usizeToU8(row_index + 1).?;
+            symbol_coordinates[1] = usizeToU8(col_index - 1).?;
+        }
+    }
+    if (row_index < 139) {
+        const bottom_middle = arr[row_index + 1][col_index];
+        if (isSymbol(bottom_middle)) {
+            symbol_coordinates[0] = usizeToU8(row_index + 1).?;
+            symbol_coordinates[1] = usizeToU8(col_index).?;
+        }
+    }
+
+    if (row_index < 139 and col_index < 139) {
+        const bottom_right = arr[row_index + 1][col_index + 1];
+        if (isSymbol(bottom_right)) {
+            symbol_coordinates[0] = usizeToU8(row_index + 1).?;
+            symbol_coordinates[1] = usizeToU8(col_index + 1).?;
+        }
+    }
+    return symbol_coordinates;
+}
+
+test "creating a struct to hold state for looping" {
+    var numStruct = NumStruct{};
+
+    try expect(numStruct.number == 0);
+
+    // push some numbers as i'm reading them.
+    numStruct.appendNumber('2');
+
+    try expect(numStruct.number == 2);
+
+    numStruct.appendNumber('2');
+
+    try expect(numStruct.number == 22);
+
+    numStruct.reset();
+
+    try expect(numStruct.number == 0);
+
+    try expect(numStruct.coordinates[0] == null);
+    try expect(numStruct.coordinates[1] == null);
+
+    var buff: [2]?u8 = [2]?u8{ 0, 2 };
+    numStruct.setCoordinates(buff);
+    try expect(numStruct.coordinates[0] == buff[0]);
+    try expect(numStruct.coordinates[1] == buff[1]);
+
+    numStruct.reset();
+    try expect(numStruct.coordinates[0] == null);
+    try expect(numStruct.coordinates[1] == null);
+}
+
+const NumStruct = struct {
+    number: u10 = 0,
+    coordinates: [2]?u8 = [2]?u8{ null, null },
+
+    fn appendNumber(self: *NumStruct, num: u8) void {
+        self.number = self.number * 10 + @as(u10, @intCast(num - '0'));
+        return;
+    }
+
+    fn getNumber(self: *NumStruct) u10 {
+        return self.number;
+    }
+
+    fn setCoordinates(self: *NumStruct, coordinates: [2]?u8) void {
+        self.coordinates = coordinates;
+        return;
+    }
+
+    fn reset(self: *NumStruct) void {
+        self.number = 0;
+        self.coordinates = [2]?u8{ null, null };
+        return;
+    }
+
+    fn coordinatesAsSlice(self: *NumStruct, buffer: *[2]u8) void {
+        buffer[0] = self.coordinates[0].?;
+        buffer[1] = self.coordinates[1].?;
+        return;
+    }
+};
+
+fn numbersAroundTokens(allocator: std.mem.Allocator, passed: []const u8) !NumberDict {
+    var dictionary = NumberDict.init(allocator);
+
+    const arr = createArrayOfCoordinates(passed);
+    var numStruct = NumStruct{};
 
     for (arr, 0..) |row, row_index| {
-        for (row, 0..) |cell, column_index| {
-            if (isNumber(cell)) {
-                number = number * 10 + @as(u10, @intCast(cell - '0'));
-                // get the 8 things around it
-                if (row_index > 0 and column_index > 0) {
-                    const top_left = arr[row_index - 1][column_index - 1];
-                    if (isSymbol(top_left)) {
-                        symbol_coordinates[0] = usizeToU8(row_index - 1).?;
-                        symbol_coordinates[1] = usizeToU8(column_index - 1).?;
-                        has_symbol = true;
-                    }
+        for (row, 0..) |column, col_index| {
+            // if a number
+            // append
+            // get coords
+            // else
+            // add to dictionary
+            // reset
+            if (isNumber(column)) {
+                numStruct.appendNumber(column);
+                if (numStruct.coordinates[0] == null) {
+                    numStruct.setCoordinates(getCoordinatesForNearestSymbol(row_index, col_index, arr));
                 }
-                if (row_index > 0) {
-                    const top_middle = arr[row_index - 1][column_index];
-                    if (isSymbol(top_middle)) {
-                        symbol_coordinates[0] = usizeToU8(row_index - 1).?;
-                        symbol_coordinates[1] = usizeToU8(column_index).?;
-                        has_symbol = true;
-                    }
-                }
-                if (row_index > 0 and column_index < 139) {
-                    const top_right = arr[row_index - 1][column_index + 1];
-                    if (isSymbol(top_right)) {
-                        symbol_coordinates[0] = usizeToU8(row_index - 1).?;
-                        symbol_coordinates[1] = usizeToU8(column_index + 1).?;
-                        has_symbol = true;
-                    }
-                }
-                if (column_index > 0) {
-                    const middle_left = arr[row_index][column_index - 1];
-                    if (isSymbol(middle_left)) {
-                        symbol_coordinates[0] = usizeToU8(row_index).?;
-                        symbol_coordinates[1] = usizeToU8(column_index - 1).?;
-                        has_symbol = true;
-                    }
-                }
-                if (column_index < 139) {
-                    const middle_right = arr[row_index][column_index + 1];
-                    if (isSymbol(middle_right)) {
-                        symbol_coordinates[0] = usizeToU8(row_index).?;
-                        symbol_coordinates[1] = usizeToU8(column_index + 1).?;
-                        has_symbol = true;
-                    }
-                }
-                if (row_index < 139 and column_index > 0) {
-                    const bottom_left = arr[row_index + 1][column_index - 1];
-                    if (isSymbol(bottom_left)) {
-                        symbol_coordinates[0] = usizeToU8(row_index + 1).?;
-                        symbol_coordinates[1] = usizeToU8(column_index - 1).?;
-                        has_symbol = true;
-                    }
-                }
-                if (row_index < 139) {
-                    const bottom_middle = arr[row_index + 1][column_index];
-                    if (isSymbol(bottom_middle)) {
-                        symbol_coordinates[0] = usizeToU8(row_index + 1).?;
-                        symbol_coordinates[1] = usizeToU8(column_index).?;
-                        has_symbol = true;
-                    }
-                }
-                if (row_index < 139 and column_index < 139) {
-                    const bottom_right = arr[row_index + 1][column_index + 1];
-
-                    if (isSymbol(bottom_right)) {
-                        symbol_coordinates[0] = usizeToU8(row_index + 1).?;
-                        symbol_coordinates[1] = usizeToU8(column_index + 1).?;
-                        has_symbol = true;
-                    }
-                }
+                continue;
             }
-
-            if (number > 0 and !isNumber(cell) and has_symbol) {
-                if (symbol_coordinates[0] != null and symbol_coordinates[1] != null) {
-                    var buffer: [2]u8 = undefined;
-                    buffer[0] = symbol_coordinates[0].?;
-                    buffer[1] = symbol_coordinates[1].?;
-
-                    // Create a slice from the buffer
-                    const slice: []const u8 = buffer[0..];
-                    try dictionary.addEntry(slice, number);
-                    number = 0;
-                    has_symbol = false;
-                    symbol_coordinates[0] = null;
-                    symbol_coordinates[1] = null;
-                }
+            if (numStruct.getNumber() > 0 and numStruct.coordinates[0] != null and numStruct.coordinates[1] != null) {
+                var buffer: [2]u8 = undefined;
+                numStruct.coordinatesAsSlice(&buffer);
+                try dictionary.addEntry(buffer[0..], numStruct.getNumber());
             }
-            if (number > 0 and !isNumber(cell) and !has_symbol) {
-                number = 0;
-                symbol_coordinates[0] = null;
-                symbol_coordinates[1] = null;
-            }
+            numStruct.reset();
         }
     }
     return dictionary;
 }
 
-fn isSymbol(char: u8) bool {
-    const period: u8 = '.';
-    return !isNumber(char) and char != period and char != '\n';
+test "tell us if something is a symbol" {
+    try expect(!isSymbol('.'));
+    try expect(isSymbol('+'));
+    try expect(isSymbol('&'));
+    try expect(isSymbol('#'));
+    try expect(isSymbol('='));
+    try expect(isSymbol('/'));
+    try expect(isSymbol('*'));
+    try expect(isSymbol('$'));
+    try expect(!isSymbol('4'));
+    try expect(!isSymbol('e'));
 }
 
-// const Grid = struct {
-//     data: ArrayList(u8),
-
-//     fn addCoordinate(self: *Grid, x: u8, y: u8, char: u8) void {
-//         _ = char;
-//         try self.data.append(generateCoordinate(x, y));
-//     }
-
-//     fn generateCoordinate(x: u8, y: u8) u8 {
-//         return y * 10 + x;
-//     }
-
-//     fn retrieveByCoordinate(x: u8, y: u8) u8 {
-//         _ = x;
-//         _ = y;
-
-//         // return the character at that position in data;
-//         return '.';
-//     }
-// };
-
-// // const Cell = struct {
-// //     x: u16,
-// //     y: u16,
-// //     character: u8,
-
-// //     fn addCoordinate(self: *Cell, x: u16, y: u16, char: u8) !void {
-// //         self.x = x;
-// //         self.y = y;
-// //         self.character = char;
-// //     }
-// // };
+fn isSymbol(char: u8) bool {
+    return !std.ascii.isAlphanumeric(char) and !std.ascii.isAlphabetic(char) and char != '\n' and char != '.';
+}
 
 fn createArrayOfCoordinates(text: []const u8) [150][150]u8 {
     var coord: [150][150]u8 = undefined;
@@ -326,7 +326,3 @@ fn createArrayOfCoordinates(text: []const u8) [150][150]u8 {
     }
     return coord;
 }
-
-// fn getCoordinates(x: u16, y: u16) u16 {
-//     return x * 10 + y;
-// }
